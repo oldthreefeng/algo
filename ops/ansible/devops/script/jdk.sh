16e267e341b3a4e00d5ae6a0c31471e105c6d6e9
louis <louisehong4168@gmail.com>
2019-11-08 20:49:56 +0800

    add ansible playbook

---
+++ ops/ansible/devops/script/jdk.sh
@@ -0,0 +1,3 @@
+JAVA_HOME=/usr/local/jdk1.8.0_181
+JRE_HOME=${JAVA_HOME}/jre
+CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar:/lib
---
+++ ops/ansible/devops/script/ulimit.sh
@@ -0,0 +1,7 @@
+ulimit -u 65535        ## max user processes
+ulimit -n 65535        ## open files
+ulimit -d unlimited    ## data seg size
+ulimit -m unlimited    ## max memory size
+ulimit -s unlimited    ## stack size
+ulimit -t unlimited    ## cpu time
+ulimit -v unlimited    ## virtual memory
