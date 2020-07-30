5f959e4a8ae2cb8feebd298d81e5f3bd63a9ebdb
louisehong <louisehong4168@gmail.com>
2020-07-30 12:16:56 +0800

    rm something

--- ops/ansible/devops/tpl/jdk.sh
+++
@@ -1,3 +0,0 @@
-JAVA_HOME=/usr/local/jdk1.8.0_181
-JRE_HOME=${JAVA_HOME}/jre
-CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar:/lib
--- ops/vpn/del.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source vars
-cd /etc/openvpn/easy-rsa/3/
-client=$1
-./easyrsa revoke $client
-./easyrsa gen-crl
-find ./ -name "$client*" -exec rm -fv {} \;
